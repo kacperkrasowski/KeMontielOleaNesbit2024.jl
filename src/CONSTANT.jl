9742e24b10cfa3133d98e8444faf3d16731a3c4e
kacperkrasowski <kacper.krasowki@carloalberto.org>
2025-05-14 17:19:35 +0200

    Creating path files

---
+++ src/CONSTANT.jl
@@ -0,0 +1,10 @@
+cwd = pwd()
+
+PDF_PATH = joinpath(cwd, "FOMC_pdf")
+PDF_PATH_TEST = joinpath(cwd, "FOMC_pdf_test")
+CACHE_PATH = joinpath(cwd, "preprocessing", "cache")
+MATRIX_PATH = joinpath(cwd, "preprocessing", "term-document matrix")
+UTILFILE_PATH = joinpath(cwd, "preprocessing", "util_files")
+PLOT_PATH = joinpath(cwd, "preprocessing", "plots")
+NMF_draws_folder = raw"D:\Robust_LDA_data\NMF_draws"
+
